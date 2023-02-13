8b08c6d7220a6c7e3bde19a3ad532127bc0635f9
evelynKr <evelynkim.kr@gmail.com>
2023-02-13 22:34:00 +0900

    styles: Navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,21 +2,29 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import { MdExplicit } from 'react-icons/md';
 import { MdAddToPhotos } from 'react-icons/md';
+import { IoBagHandleOutline } from 'react-icons/io5';
 
 export default function Navbar() {
   return (
-    <header>
-      <Link to='/'>
+    <header className='flex flex-col'>
+      <Link
+        to='/'
+        className='flex items-center text-5xl text-black font-bold my-8 pr-3'
+      >
         <MdExplicit />
         <h1>EDITORIAL</h1>
       </Link>
-      <nav>
+      <nav className='flex justify-between gap-4 bg-black text-white font-semibold p-4 pr-5'>
         <Link to='/products'>Products</Link>
-        <Link to='/carts'>Carts</Link>
-        <Link to='/products/new'>
-          <MdAddToPhotos />
-        </Link>
-        <button>Login</button>
+        <div className='flex flex-row gap-4'>
+          <Link to='/carts' className='text-2xl flex justify-end'>
+            <IoBagHandleOutline />
+          </Link>
+          <Link to='/products/new' className='text-2xl flex justify-end'>
+            <MdAddToPhotos />
+          </Link>
+          <button>Log in</button>
+        </div>
       </nav>
     </header>
   );
