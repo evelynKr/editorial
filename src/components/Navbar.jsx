2aad8298440ae5a69bdb3b5b4d44b363ad47d5f0
evelynKr <evelynkim.kr@gmail.com>
2023-02-13 23:56:54 +0900

    feat: user avatar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,6 +4,7 @@ import { MdExplicit } from 'react-icons/md';
 import { MdAddToPhotos } from 'react-icons/md';
 import { IoBagHandleOutline } from 'react-icons/io5';
 import { login, logout, onUserStateChange } from './../api/firebase';
+import User from './User';
 
 export default function Navbar() {
   const [user, setUser] = useState();
@@ -12,23 +13,24 @@ export default function Navbar() {
     onUserStateChange(setUser);
   }, []);
   return (
-    <header className='flex flex-col'>
+    <header className='flex flex-col content-center'>
       <Link
         to='/'
-        className='flex items-center text-5xl text-black font-bold my-8 pr-3'
+        className='flex items-center content-center text-5xl text-black font-bold my-8 pr-3'
       >
         <MdExplicit />
         <h1>EDITORIAL</h1>
       </Link>
-      <nav className='flex justify-between gap-4 bg-black text-white font-semibold p-4 pr-5'>
+      <nav className='flex justify-between items-center gap-4 bg-black text-white font-semibold p-4 pr-5'>
         <Link to='/products'>Products</Link>
-        <div className='flex flex-row gap-4'>
-          <Link to='/carts' className='text-2xl flex justify-end'>
+        <div className='flex flex-row gap-4 items-center'>
+          <Link to='/carts' className='text-2xl flex'>
             <IoBagHandleOutline />
           </Link>
-          <Link to='/products/new' className='text-2xl flex justify-end'>
+          <Link to='/products/new' className='text-2xl flex'>
             <MdAddToPhotos />
           </Link>
+          {user && <User user={user} />}
           {!user && <button onClick={login}>Log in</button>}
           {user && <button onClick={logout}>Log out</button>}
         </div>
