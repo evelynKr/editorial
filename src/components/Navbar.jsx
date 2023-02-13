95e0765ddd9a531110c1196c9f056f1286fe1bfd
evelynKr <evelynkim.kr@gmail.com>
2023-02-14 01:30:28 +0900

    feat: Add Button Component and design: navbar, adminUser UI

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,6 +5,7 @@ import { MdAddToPhotos } from 'react-icons/md';
 import { IoBagHandleOutline } from 'react-icons/io5';
 import { login, logout, onUserStateChange } from './../api/firebase';
 import User from './User';
+import Button from './ui/Button';
 
 export default function Navbar() {
   const [user, setUser] = useState();
@@ -19,23 +20,25 @@ export default function Navbar() {
     <header className='flex flex-col content-center'>
       <Link
         to='/'
-        className='flex items-center content-center text-5xl text-black font-bold my-8 pr-3'
+        className='flex items-center content-center bg-brand text-5xl text-white font-bold py-8 pr-3'
       >
         <MdExplicit />
         <h1>EDITORIAL</h1>
       </Link>
-      <nav className='flex justify-between items-center gap-4 bg-black text-white font-semibold p-4 pr-5'>
+      <nav className='flex justify-between items-center gap-4 border-b border-brand text-black font-semibold p-4 pr-5'>
         <Link to='/products'>Products</Link>
         <div className='flex flex-row gap-4 items-center'>
           <Link to='/carts' className='text-2xl flex'>
             <IoBagHandleOutline />
           </Link>
-          <Link to='/products/new' className='text-2xl flex'>
-            <MdAddToPhotos />
-          </Link>
+          {user && user.isAdmin && (
+            <Link to='/products/new' className='text-2xl flex'>
+              <MdAddToPhotos />
+            </Link>
+          )}
           {user && <User user={user} />}
-          {!user && <button onClick={login}>Log in</button>}
-          {user && <button onClick={logout}>Log out</button>}
+          {!user && <Button text={'Login'} onClick={login} />}
+          {user && <Button text={'Logout'} onClick={logout} />}
         </div>
       </nav>
     </header>
