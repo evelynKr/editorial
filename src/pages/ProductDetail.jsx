f811bbaa74737788a3875eee7e7298f211772fd2
evelynKr <evelynkim.kr@gmail.com>
2023-02-17 01:11:11 +0900

    feat: ProductDetail Page

--- src/pages/ProductDetail.jsx
+++ src/pages/ProductDetail.jsx
@@ -1,5 +1,36 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { useLocation } from 'react-router-dom';
+import Button from './../components/ui/Button';
 
 export default function ProductDetail() {
-  return <div>ProductDetail</div>;
+  const {
+    state: {
+      product: { id, image, title, description, category, price, options },
+    },
+  } = useLocation();
+  const [selected, setSelected] = useState(options && options[0]);
+  const handleSelect = e => setSelected(e.target.value);
+  const handleClick = e => {
+    // 여기 장바구니에 추가하면 된다
+  };
+
+  return (
+    <section>
+      <p>{category}</p>
+      <img src={image} alt={title} />
+      <div>
+        <h2>{title}</h2>
+        <p>₩{price}</p>
+        <p>{description}</p>
+        <p>옵션:</p>
+        <select onChange={handleSelect} value={selected}>
+          {options &&
+            options.map((option, index) => (
+              <option key={index}>{option}</option>
+            ))}
+        </select>
+        <Button text='장바구니에 추가' onClick={handleClick} />
+      </div>
+    </section>
+  );
 }
